faff44c38b613f8af8d083bc39e28b66e828cbc6
Richard Saseun <richardsaseun@gmail.com>
2022-09-17 10:41:50 +0100

    bug: fix import issue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
-<script setup>
-// import { RouterLink, RouterView } from "vue-router";
-import AppSidebar from "./components/AppSideBar.vue";
+<script>
+import { RouterView } from "vue-router";
+import AppSideNavigation from "./components/AppSideNavigation.vue";
+
+export default {
+  components: {
+    AppSideNavigation,
+    RouterView,
+  },
+};
 </script>
 
 <template>
-  <AppSidebar></AppSidebar>
-  <!-- <header> -->
-  <!-- <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav> -->
-  <!-- </header> -->
-
-  <!-- <div class="page"> -->
+  <AppSideNavigation></AppSideNavigation>
   <RouterView />
-  <!-- </div> -->
 </template>
 
 <style scoped></style>
